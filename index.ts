f2c54bbc97d0099c15f018623b84cbc2befdb54d
Oliwier Michalik <oliwier.michalik9@gmail.com>
2025-09-25 16:52:50 +0200

    fix: reexport defaults and not whole package

--- index.ts
+++ index.ts
@@ -1,3 +1,3 @@
-export * as fsRouterPlugin from './internal/fs-router-plugin';
-export * as generateCHeaderPlugin from './internal/generate-c-header-plugin';
-export * as staticPreviewPlugin from './internal/static-preview-plugin';
+export { default as fsRouterPlugin } from './internal/fs-router-plugin'
+export { default as staticPreviewPlugin } from './internal/static-preview-plugin'
+export { default as generateCHeaderPlugin } from './internal/generate-c-header-plugin'
